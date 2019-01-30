e7311da0e783341bd510758b1bce3233f07408f4
joepak <josephpak2014@gmail.com>
2019-01-30 15:12:49 -0800

    More consistent open animation

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,20 @@
 
-const toggleMenu = () => {
+// const toggleMenu = () => {
+//   // Toggle the "menu--open" class on your menu reference. 
+//   menu.classList.toggle("menu--open");
+
+//   TweenMax.to(menu, .3, {
+//     width: "350px",
+//     ease: Bounce.easeout
+//   })
+// }
+
+
+const openMenu = () => {
   // Toggle the "menu--open" class on your menu reference. 
   menu.classList.toggle("menu--open");
+  openButton.classList.toggle("hide-btn")
+  closeButton.classList.toggle("hide-btn")
 
   TweenMax.to(menu, .3, {
     width: "350px",
@@ -9,9 +22,23 @@ const toggleMenu = () => {
   })
 }
 
+const closeMenu = () => {
+  // Toggle the "menu--open" class on your menu reference.
+  menu.classList.toggle("menu--open"); 
+  openButton.classList.toggle("hide-btn")
+  closeButton.classList.toggle("hide-btn")
+
+  TweenMax.to(menu, .3, {
+    width: "0px",
+    ease: Bounce.easeout
+  })
+}
+
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector(".menu");
 // create a reference to the ".menu-button" class
-const menuButton = document.querySelector(".menu-button");
+const openButton = document.querySelector(".openBtn");
+const closeButton = document.querySelector(".closeBtn");
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener("click", toggleMenu);
+openButton.addEventListener("click", openMenu);
+closeButton.addEventListener("click", closeMenu);
